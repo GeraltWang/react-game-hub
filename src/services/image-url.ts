8408800c392a2e14c22a5b15f362d9716e6a56ec
王昶 <wgeralt@outlook.com>
2023-10-24 14:37:16 +0800

    handle game without image

--- src/services/image-url.ts
+++ src/services/image-url.ts
@@ -1,4 +1,7 @@
+import defaultPlaceHolder from '../assets/no-image-placeholder.webp'
+
 const getCroppedImageUrl = (url: string) => {
+	if (!url) return defaultPlaceHolder
   // 根据 url 中 media/ 的位置，截取 url 的前半部分，然后拼接 crop/600/400/，再拼接 url 的后半部分
 	const target = 'media/'
 	const index = url.indexOf(target) + target.length

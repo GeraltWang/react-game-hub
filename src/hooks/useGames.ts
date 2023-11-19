fad63778cdd1fd700f2004f51296808fa638d21f
GeraltWang <WGERALT@OUTLOOK.COM>
2023-11-19 20:58:44 +0800

    refactor: ♻️ remove duplicate interface

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -2,6 +2,7 @@
 import { GameQuery } from '../App'
 import { useQuery } from '@tanstack/react-query'
 import apiClient, { FetchResponse } from '../services/api-client'
+import { Platform } from './usePlatforms'
 
 export interface Game {
 	id: number
@@ -10,12 +11,6 @@ export interface Game {
 	parent_platforms: { platform: Platform }[]
 	metacritic: number
 	rating_top: number
-}
-
-export interface Platform {
-	id: number
-	name: string
-	slug: string
 }
 
 const useGames = (gameQuery: GameQuery) =>

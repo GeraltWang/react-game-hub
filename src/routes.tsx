103afcbb78022a4c7a7ccf7fe913654e77b79742
GeraltWang <wgeralt@outlook.com>
2023-12-04 16:56:13 +0800

    feat: ✨ setting up routes and handle error

---
+++ src/routes.tsx
@@ -0,0 +1,25 @@
+import { createBrowserRouter } from 'react-router-dom'
+import GameDetailPage from './pages/GameDetailPage'
+import HomePage from './pages/HomePage'
+import Layout from './pages/Layout'
+import ErrorPage from './pages/ErrorPage'
+
+const router = createBrowserRouter([
+	{
+		path: '/',
+		element: <Layout></Layout>,
+		errorElement: <ErrorPage></ErrorPage>,
+		children: [
+			{
+				index: true,
+				element: <HomePage></HomePage>,
+			},
+			{
+				path: 'game/:id',
+				element: <GameDetailPage></GameDetailPage>,
+			},
+		],
+	},
+])
+
+export default router

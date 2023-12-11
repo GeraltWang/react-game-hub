adc7bf1846d98d422a56d199d3552e4b28bbb080
GeraltWang <WGERALT@OUTLOOK.COM>
2023-12-11 22:28:07 +0800

    refactor: ♻️ move entity interfaces

--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -2,14 +2,7 @@ import { useQuery } from '@tanstack/react-query'
 import ms from 'ms'
 import initialGenres from '../data/genres'
 import APIClient from '../services/api-client'
-
-export interface Genre {
-	id: number
-	name: string
-	slug: string
-	games_count: number
-	image_background: string
-}
+import { Genre } from '../entities/Genre'
 
 const apiClient = new APIClient<Genre>('/genres')
 
